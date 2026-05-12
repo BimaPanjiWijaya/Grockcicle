e838007a31ea5676844b44839149781833eca32c
unknown <bimapanjiwijaya@gmail.com>
2026-05-12 10:26:50 +0700

    feat: add ProductPage component and update JSON data for various products

--- grockcicle/app/page.tsx
+++ grockcicle/app/page.tsx
@@ -1,9 +1,11 @@
 import HomeBanner from "@/components/HomeBanner";
+// import HomeDetailInfo from "@/components/HomeDetailInfo";
 
 export default function Home() {
   return (
     <main className="flex-1">
       <HomeBanner />
+      {/* <HomeDetailInfo /> */}
     </main>
   );
 }
