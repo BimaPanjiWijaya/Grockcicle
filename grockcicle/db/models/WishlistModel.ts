f2a08ec46ea3cd68118ecd57e9eb93c9b5a1ae70
unknown <bimapanjiwijaya@gmail.com>
2026-05-14 22:56:40 +0700

    feat: implement proxy middleware for wishlist API with JWT authentication and user ID header

---
+++ grockcicle/db/models/WishlistModel.ts
@@ -0,0 +1,37 @@
+import { database } from "../config/mongodb";
+import { ObjectId } from "mongodb";
+
+class WishlistModel {
+  static collection() {
+    return database.collection("wishlists");
+  }
+  static async getWishlistByUserId(userId: string) {
+    return await this.collection().find({ userId }).toArray();
+  }
+
+  static async add(userId: string, productId: string) {
+    const existing = await this.collection().findOne({ userId, productId });
+    if (existing) {
+      throw { status: 400, message: "Product already in wishlist" };
+    }
+    const result = await this.collection().insertOne({
+      userId,
+      productId,
+      createdAt: new Date(),
+    });
+    return result;
+  }
+
+  static async remove(wishlistId: string, userId: string) {
+    const result = await this.collection().deleteOne({
+      _id: new ObjectId(wishlistId),
+      userId,
+    });
+    if (result.deletedCount === 0) {
+      throw { status: 404, message: "Whishlist item  not found" };
+    }
+    return result;
+  }
+}
+
+export default WishlistModel;
---
+++ grockcicle/proxy.ts
@@ -0,0 +1,45 @@
+import { cookies } from "next/headers";
+import errorHandler from "./helpers/errorHandler";
+import { verify } from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("Authorization")?.value;
+  if (request.nextUrl.pathname.startsWith("/api")) {
+    try {
+      const [bearer, accessToken] = token?.split(" ") || [];
+
+      if (bearer !== "Bearer" || !accessToken) {
+        throw { message: "Please login first", status: 401 };
+      }
+
+      const secretKey = process.env.SERCRET_KEY || " default_secret_key";
+      const decoded = verify(accessToken, secretKey) as { userId: string };
+      const userId = decoded.userId;
+
+      const requestHeaders = new Headers(request.headers);
+      requestHeaders.set("X-User-Id", userId);
+
+      const response = NextResponse.next({
+        request: {
+          headers: requestHeaders,
+        },
+      });
+      console.log("User ID set in header:", userId);
+      return response;
+    } catch (error) {
+      return errorHandler(error);
+    }
+  }
+  if (request.nextUrl.pathname.startsWith("/wishlists")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+    return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: ["/api/wishlists/:path*", "/wishlists/:path*"],
+};
